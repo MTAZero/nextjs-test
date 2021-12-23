c481f6906a9c5ec8154f6ea3346a2e7a379406b2
Bùi Xuân Thủy <buithuy3996@gmail.com>
2021-12-23 07:31:40 +0700

    fix animation

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -6,6 +6,81 @@ import { useActions } from '../../redux';
 import styles from '../../styles/Post.module.scss';
 import { AiTwotoneHeart } from 'react-icons/ai';
 
+const Post = (props: any) => {
+    const dispatch = useDispatch();
+    const actions = useActions();
+
+    let post = props.post;
+    const userInfo = useSelector((state: any) => state.auth.userInfo);
+
+    let [buttonState, setButtonState] = useState('normal');
+
+    const renderUserInfo = () => {
+        return (
+            <div className={styles.userInfo}>
+                <img
+                    className={styles.userAvatar}
+                    src={userInfo.avatar}
+                    alt="avatar"
+                />
+                <div className={styles.userFullname}>{userInfo.fullname}</div>
+            </div>
+        );
+    };
+
+    const getClassName = (state: any) => {
+        if (state == 'press') return styles.LikeButtonPress;
+
+        return styles.LikeButton;
+    };
+
+    const handleOnClick = () => {
+        setButtonState('press');
+
+        setTimeout(() => {
+            setButtonState('normal');
+        }, 1000);
+    };
+
+    return (
+        <div className={styles.Feed}>
+            {renderUserInfo()}
+            <div className={styles.FeedContent}>
+                {post.image && (
+                    <img src={post.image} className={styles.image} alt="img" />
+                )}
+                {post.content}
+            </div>
+            <div className={styles.FeedControl}>
+                <div
+                    style={{
+                        height: 40,
+                        width: 40,
+                        borderRadius: 20,
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        // backgroundColor: 'yellow'
+                    }}
+                >
+                    <div
+                        className={getClassName(buttonState)}
+                        onClick={() => {
+                            handleOnClick();
+                            dispatch(actions.PostActions.likePost(post._id));
+                        }}
+                    >
+                        <AiTwotoneHeart className={styles.IconButton} />
+
+                        <div className={styles.textHide}>liked!</div>
+                    </div>
+                </div>
+                <div className={styles.num_like}>{post.num_like}</div>
+            </div>
+        </div>
+    );
+};
+
 const Posts: NextPage = () => {
     const dispatch = useDispatch();
     const posts = useSelector((state: any) => state.posts.posts);
@@ -84,56 +159,7 @@ const Posts: NextPage = () => {
 
             <div className={styles.ListFeed}>
                 {posts.map((post: any, index: any) => {
-                    return (
-                        <div className={styles.Feed}>
-                            {renderUserInfo()}
-                            <div className={styles.FeedContent}>
-                                {post.image && (
-                                    <img
-                                        src={post.image}
-                                        className={styles.image}
-                                        alt="img"
-                                    />
-                                )}
-                                {post.content}
-                            </div>
-                            <div className={styles.FeedControl}>
-                                <div
-                                    style={{
-                                        height: 35,
-                                        width: 35,
-                                        borderRadius: 18,
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        justifyContent: 'center',
-                                        // backgroundColor: 'yellow'
-                                    }}
-                                >
-                                    <div
-                                        className={styles.LikeButton}
-                                        onClick={() => {
-                                            dispatch(
-                                                actions.PostActions.likePost(
-                                                    post._id,
-                                                ),
-                                            );
-                                        }}
-                                    >
-                                        <AiTwotoneHeart
-                                            className={styles.IconButton}
-                                        />
-
-                                        <div className={styles.textHide}>
-                                            liked!
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className={styles.num_like}>
-                                    {post.num_like}
-                                </div>
-                            </div>
-                        </div>
-                    );
+                    return <Post post={post} />;
                 })}
             </div>
         </div>
